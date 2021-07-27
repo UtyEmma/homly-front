0e947f96d3d1584d3269b2a3f299ad258ba05e25
Ebube Okorie <okorieebube1@gmail.com>
2021-07-27 05:05:42 -1100

    merge changes

--- src/providers/guards/agent-guard.js
+++ src/providers/guards/agent-guard.js
@@ -3,19 +3,24 @@ import { Redirect, Route } from "react-router-dom";
 
 function AgentRoute({ component: Component, ...restOfProps }) {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
-  const user = localStorage.getItem('user')
-  const type = localStorage.getItem('type');
+  const user = localStorage.getItem("user");
+  const type = localStorage.getItem("type");
   const agent_object = JSON.parse(user);
   let auth = false;
 
-  if(isAuthenticated && type){ auth = true; }
+  if (isAuthenticated && type) {
+    auth = true;
+  }
 
   return (
     <Route
       {...restOfProps}
       render={(props) =>
-        isAuthenticated && user === 'agent' 
-          ? <Component {...props} agent={agent_object} /> : <Redirect to="/agent-login" />
+        isAuthenticated && user === "agent" ? (
+          <Component {...props} agent={agent_object} />
+        ) : (
+          <Redirect to="/agent-login" />
+        )
       }
     />
   );
