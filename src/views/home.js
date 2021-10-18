797c3ec8c650b23349d1bf53839c4fe006ed392e
KelvinIOT <53199415+okorieebube@users.noreply.github.com>
2021-10-18 04:38:34 -1100

    commit

--- src/views/home.js
+++ src/views/home.js
@@ -75,12 +75,10 @@ const Home = ({ isLoggedIn, user, isLoading, setIsLoading, status, token }) => {
                 <meta name="twitter:creator" content="@" />
                 <meta name="twitter:title" content="Bayof - Take ownership of landed property, globally" />
                 <meta name="twitter:description" content="We're using technology to take the housing and real estate industry into the modern age" />
-                {/* <meta name="twitter:image" content="/images/homeid-social-logo.png" /> */}
                 <meta property="og:url" content={process.env.REACT_APP_API_URL} />
                 <meta property="og:title" content="Take ownership of landed property, globally" />
                 <meta property="og:description" content="We're using technology to take the real estate industry into the future" />
                 <meta property="og:type" content="website" />
-                {/* <meta property="og:image" content="/images/homeid-social.png" /> */}
                 <meta property="og:image:type" content="image/png" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
