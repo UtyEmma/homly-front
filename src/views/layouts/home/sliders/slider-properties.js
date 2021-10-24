b79c30ead8b38b095592d88ef011d939e226fa06
Utibe-Abasi Akpan <utyemma@gmail.com>
2021-10-24 12:59:22 +0100

    Properties Slider Fix

--- src/views/layouts/home/sliders/slider-properties.js
+++ src/views/layouts/home/sliders/slider-properties.js
@@ -7,7 +7,7 @@ export const SliderProperties = ({listings}) => {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 3,
         autoplay: true,
         arrows: true,
